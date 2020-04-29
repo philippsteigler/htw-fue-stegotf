1287b8683c9f956ee6753527a785f95a98006ff7
Philipp Steigler <philipp@mbp.local>
2020-04-29 23:21:03 +0200

    Change model layers

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import pathlib
 import os
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
-BB_HOME = pathlib.Path("/tmp/BOSSbase")
 
-class_names = np.array([item.name for item in BB_HOME.glob("train/*") if item.name != ".DS_Store"])
-image_count = len(list(BB_HOME.glob("train/*/*.pgm")))
-
-BATCH_SIZE = 32
-EPOCHS = 10
-STEPS_PER_EPOCH = np.ceil(image_count/BATCH_SIZE)
+bb_path = pathlib.Path("/Users/philipp/BOSSbase")
+img_count = len(list(bb_path.glob("train/*/*.pgm")))
+img_width = 512
+img_height = 512
+batch_size = 32
+epochs = 10
+steps_per_epoch = np.ceil(img_count / batch_size)
 
 kernel_hp = np.array(
     [[[ -1], [  2], [ -2], [  2], [ -1]],
@@ -25,16 +25,15 @@ kernel_hp = np.array(
 
 def get_label(file_path):
   parts = tf.strings.split(file_path, os.path.sep)
-  if parts[-2] == "cover": return np.array([0, 1])
-  else: return np.array([1, 0])
-  #return parts[-2] == class_names
+  if parts[-2] == "cover": return 0
+  else: return 1
 
 def get_image(file_path):
   image = keras.preprocessing.image.load_img(file_path, color_mode="grayscale")
   image = keras.preprocessing.image.img_to_array(image) / 255.0
   image = ndimage.convolve(image, kernel_hp)
-  image = np.delete(image, [0, 1, 510, 511], 0)
-  image = np.delete(image, [0, 1, 510, 511], 1)
+  image = np.delete(image, [0, 1, img_width - 2, img_width - 1], 0)
+  image = np.delete(image, [0, 1, img_height - 2, img_height - 1], 1)
   return image
 
 def process_path(file_path):
@@ -43,31 +42,26 @@ def process_path(file_path):
   return image, label
 
 def set_shapes(img, label):
-  img.set_shape((508, 508, 1))
-  label.set_shape((2))
+  img.set_shape((img_width - 4, img_height - 4, 1))
+  label.set_shape(())
   return img, label
 
 if __name__ == "__main__":
-  ds_train_list = tf.data.Dataset.list_files(os.path.join(BB_HOME, "train/*/*.pgm"))
+  ds_train_list = tf.data.Dataset.list_files(os.path.join(bb_path, "train/*/*.pgm"))
   train_dataset = ds_train_list.map(lambda x: tf.numpy_function(process_path, [x], [tf.float32, tf.int64]), num_parallel_calls=AUTOTUNE)
   train_dataset = train_dataset.map(lambda i, l: set_shapes(i, l))
 
   print("\nDATASET:", train_dataset.element_spec)
 
-  for image, label in train_dataset.take(1):
-    print("\nIMAGE SHAPE:", image)
-    print("\nLABEL SHAPE:", label)
-
   model = keras.Sequential([
-    keras.layers.Conv2D(64, (7, 7), strides=2, padding="same", activation="relu", input_shape=(508, 508, 1)),
+    keras.layers.Conv2D(64, (7, 7), padding="same", activation="relu", input_shape=(img_width - 4, img_height - 4, 1)),
+    keras.layers.MaxPool2D((2, 2)),
     keras.layers.Conv2D(16, (5, 5), padding="same", activation="relu"),
     keras.layers.MaxPool2D((2, 2)),
-    keras.layers.Dropout(0.25),
     keras.layers.Flatten(),
     keras.layers.Dense(128, activation="relu"),
     keras.layers.Dense(128, activation="relu"),
-    keras.layers.Dropout(0.5),
-    keras.layers.Dense(2, activation="softmax")
+    keras.layers.Dense(1)
   ])
 
   model.compile(
@@ -77,6 +71,5 @@ if __name__ == "__main__":
   )
 
   print("\nMODEL:", model.summary())
-  train_dataset = train_dataset.shuffle(image_count).repeat(EPOCHS).batch(BATCH_SIZE)
-  model.fit(train_dataset, epochs=EPOCHS, steps_per_epoch=STEPS_PER_EPOCH)
-  
+  train_dataset = train_dataset.shuffle(img_count).repeat(epochs).batch(batch_size)
+  model.fit(train_dataset, epochs=epochs, steps_per_epoch=steps_per_epoch)
