95455c75a56151de3b1f9e2fffb351e80a435473
Philipp Steigler <philipp@mbp.local>
2020-06-22 00:06:51 +0200

    Implement undecimated DCT to preprocess images

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from PIL import Image
 import jpegio as jio
 import os
 import random
+import math
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
-input_size = 15000 # for each category
+input_size = 10000 # for each category
 tt_ratio = 0.9
 
 base_path = "/Users/philipp/ALASKA2"
@@ -41,7 +42,7 @@ print("\nTEST DATA:", len(test_filenames))
 img_width = 512
 img_height = 512
 batch_size = 32
-epochs = 25
+epochs = 20
 steps_per_epoch = train_img_count // batch_size
 
 def get_label(file_path):
@@ -53,26 +54,53 @@ def get_label(file_path):
   return label
 
 def get_img_as_ycbcr(file_path):
-  image = Image.open(file_path)
-  image.draft("YCbCr", None)
-  image.load()
-  image = np.array(image) / 255.
-  return image
+  jpeg = Image.open(file_path)
+  jpeg.draft("YCbCr", None)
+  jpeg.load()
+  jpeg = np.array(jpeg) / 255.
 
-def get_img_as_dct_coef(file_path):
-  jpeg = jio.read(file_path)
-  image = np.dstack(jpeg.coef_arrays)
-  image = np.absolute(image) / 128.
-  return image
+  y = jpeg[:, :, 0]
+  y = y[:, :, np.newaxis]
+  return y
+
+def get_img_as_ycbcr_raw(file_path):
+  jpegStruct = jio.read(file_path)
+
+  [col, row] = np.meshgrid(range(8), range(8))
+  T = 0.5 * np.cos(np.pi * (2 * col + 1) * row / (2 * 8))
+  T[0, :] = T[0, :] / np.sqrt(2)
+
+  img_dims = np.array(jpegStruct.coef_arrays[0].shape)
+  n_blocks = img_dims // 8
+  broadcast_dims = (n_blocks[0], 8, n_blocks[1], 8)
+  
+  dct_coeffs = jpegStruct.coef_arrays[0]
+  QM = jpegStruct.quant_tables[0]
+  
+  t = np.broadcast_to(T.reshape(1, 8, 1, 8), broadcast_dims)
+  qm = np.broadcast_to(QM.reshape(1, 8, 1, 8), broadcast_dims)
+  dct_coeffs = dct_coeffs.reshape(broadcast_dims)
+  
+  a = np.transpose(t, axes=(0, 2, 3, 1))
+  b = (qm * dct_coeffs).transpose(0, 2, 1, 3)
+  c = t.transpose(0, 2, 1, 3)
+          
+  z = a @ b @ c
+  z = z.transpose(0, 2, 1, 3)
+  z = z.reshape(img_dims)
+  
+  y = z[:, :, np.newaxis] 
+  y = y / 128.
+  return y
 
 def process_path(file_path):
   file_path = file_path.decode("utf-8")
   label = get_label(file_path)
-  image = get_img_as_dct_coef(file_path)
+  image = get_img_as_ycbcr_raw(file_path)
   return image, label
 
 def set_shapes(image, label):
-  image.set_shape((512, 512, 3))
+  image.set_shape((512, 512, 1))
   label.set_shape((1))
   return image, label
 
@@ -82,50 +110,68 @@ def load_dataset(filenames):
   dataset = dataset.map(lambda i, l: set_shapes(i, l))
   return dataset
 
+def dctr_filter(shape, dtype=None):
+  B = np.zeros(shape=(8, 8, 8, 8)) # k, l; m, n
+  for k in range(0, 8):
+    for l in range(0, 8):
+      if k == 0: wk = 1 / math.sqrt(2)
+      else: wk = 1
+      if l == 0: wl = 1 / math.sqrt(2)
+      else: wl = 1
+
+      for m in range(0, 8):
+        for n in range(0, 8):
+          B[k][l][m][n] = (wk * wl / 4) * math.cos((math.pi * k * (2*m + 1)) / 16) * math.cos((math.pi * l * (2*n + 1)) / 16)
+
+  B = B.reshape(64, 8, 8)
+  B = np.expand_dims(B, -1)
+  B = B.transpose(1, 2, 3, 0)
+  assert B.shape == shape
+  return tf.keras.backend.variable(B, dtype="float32")
+
 def generate_model():
   model = keras.Sequential([
     # type 1
-    keras.layers.Conv2D(64, 7, padding="same", kernel_initializer="he_normal", strides=2, input_shape=(img_height, img_width, 3)),
-    keras.layers.LeakyReLU(alpha=0.2),
-    keras.layers.BatchNormalization(),
+    keras.layers.Conv2D(64, 8, padding="same", kernel_initializer=dctr_filter, strides=2, use_bias=False, trainable=False, input_shape=(img_height, img_width, 1)),
+    keras.layers.Lambda(lambda x: tf.abs(x)),
 
     # type 2
     keras.layers.Conv2D(16, 3, padding="same", kernel_initializer="he_normal"),
-    keras.layers.LeakyReLU(alpha=0.2),
     keras.layers.BatchNormalization(),
+    keras.layers.LeakyReLU(alpha=0.2),
     keras.layers.MaxPool2D(3, strides=2, padding="same"),
 
     keras.layers.Conv2D(32, 3, padding="same", kernel_initializer="he_normal"),
-    keras.layers.LeakyReLU(alpha=0.2),
     keras.layers.BatchNormalization(),
+    keras.layers.LeakyReLU(alpha=0.2),
     keras.layers.MaxPool2D(3, strides=2, padding="same"),
 
     keras.layers.Conv2D(64, 3, padding="same", kernel_initializer="he_normal"),
-    keras.layers.LeakyReLU(alpha=0.2),
     keras.layers.BatchNormalization(),
+    keras.layers.LeakyReLU(alpha=0.2),
     keras.layers.MaxPool2D(3, strides=2, padding="same"),
 
     keras.layers.Conv2D(128, 3, padding="same", kernel_initializer="he_normal"),
-    keras.layers.LeakyReLU(alpha=0.2),
     keras.layers.BatchNormalization(),
+    keras.layers.LeakyReLU(alpha=0.2),
     keras.layers.MaxPool2D(3, strides=2, padding="same"),
 
     keras.layers.Conv2D(256, 3, padding="same", kernel_initializer="he_normal"),
-    keras.layers.LeakyReLU(alpha=0.2),
     keras.layers.BatchNormalization(),
+    keras.layers.LeakyReLU(alpha=0.2),
     keras.layers.MaxPool2D(3, strides=2, padding="same"),
 
     # type 3
     keras.layers.Conv2D(512, 3, padding="same", kernel_initializer="he_normal"),
-    keras.layers.LeakyReLU(alpha=0.2),
     keras.layers.BatchNormalization(),
+    keras.layers.LeakyReLU(alpha=0.2),
     keras.layers.GlobalAveragePooling2D(),
 
-    keras.layers.Dense(2048),
+    keras.layers.Dense(512),
     keras.layers.LeakyReLU(alpha=0.2),
     keras.layers.Dropout(0.5),
 
-    keras.layers.Dense(1024),
+    keras.layers.Dense(256),
     keras.layers.LeakyReLU(alpha=0.2),
     keras.layers.Dropout(0.5),
 
@@ -168,11 +214,12 @@ if __name__ == "__main__":
   # train the model
   train_dataset = train_dataset.repeat(epochs).batch(batch_size)
   cp_callback = create_cp_callback()
-  model.fit(train_dataset, batch_size=batch_size, epochs=epochs, steps_per_epoch=steps_per_epoch, callbacks=[cp_callback])
+  model.fit(train_dataset, epochs=epochs, steps_per_epoch=steps_per_epoch, callbacks=[cp_callback])
 
   # save trained model with weights
   model.save(base_path + "/TF/model")
   
   # evaluate the model
   test_dataset = test_dataset.batch(batch_size)
-  model.evaluate(test_dataset, batch_size=batch_size)
+  model.evaluate(test_dataset)
+  
