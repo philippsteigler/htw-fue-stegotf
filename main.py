57d735494bae7c5f8d4acbbf7ba6b4825381297e
Philipp Steigler <philipp.steigler@mailbox.org>
2020-11-22 22:32:11 +0100

    Add validation dataset during training

--- main.py
+++ main.py
@@ -15,22 +15,28 @@ img_height = 512
 batch_size = 32
 epochs = 10
 image_count = len(os.listdir(cover_path) + os.listdir(stego_path))
-steps_per_epoch = image_count // batch_size
 
 if __name__ == "__main__":
-  # load images from directory
-  train_datagen = keras.preprocessing.image.ImageDataGenerator(
+  # create train and validation dataset
+  image_datagen = keras.preprocessing.image.ImageDataGenerator(
     rescale=1./255,
-    horizontal_flip=True,
-    vertical_flip=True)
+    validation_split=0.2)
 
-  train_generator = train_datagen.flow_from_directory(
+  train_generator = image_datagen.flow_from_directory(
     train_path,
     target_size=(img_height, img_width),
     class_mode="binary",
-    batch_size=batch_size)
+    batch_size=batch_size,
+    subset="training")
 
-  # Load EfficientNet
+  valid_generator = image_datagen.flow_from_directory(
+    train_path,
+    target_size=(img_height, img_width),
+    class_mode="binary",
+    batch_size=batch_size,
+    subset="validation")
+
+  # load EfficientNet as base
   conv_base = efn.EfficientNetB0(
     weights="imagenet",
     include_top=False,
@@ -42,12 +48,12 @@ if __name__ == "__main__":
   model = keras.Sequential()
   model.add(conv_base)
 
-  # Add custom top layers for classification
+  # add custom top layers for classification
   model.add(keras.layers.GlobalAveragePooling2D())
   model.add(keras.layers.Dropout(0.5))
   model.add(keras.layers.Dense(1, activation="sigmoid"))
 
-  # Compile final model
+  # compile final model
   model.compile(
     optimizer="adam",
     loss="binary_crossentropy",
@@ -56,4 +62,9 @@ if __name__ == "__main__":
   print(model.summary())
 
   # train model
-  model.fit_generator(train_generator, epochs=epochs, steps_per_epoch=steps_per_epoch)
+  model.fit_generator(
+    train_generator,
+    steps_per_epoch = train_generator.samples // batch_size,
+    validation_data = valid_generator,
+    validation_steps = valid_generator.samples // batch_size,
+    epochs = epochs)
