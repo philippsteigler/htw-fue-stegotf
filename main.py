31ec8c19dd9e61eccd6f10b80f1956079f8d467b
Philipp Steigler <philipp.steigler@mailbox.org>
2020-12-16 19:32:22 +0100

    Add callbacks

--- main.py
+++ main.py
@@ -10,7 +10,25 @@ img_height = 512
 batch_size = 32
 epochs = 20
 
+# Function for decaying the learning rate
+def decay(epoch):
+  if epoch < 4:
+    return 1e-3
+  elif epoch >= 4 and epoch < 12:
+    return 1e-4
+  else:
+    return 1e-5
+
+# Callback for printing the LR at the end of each epoch
+class PrintLR(keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs=None):
+    print("Learning rate for epoch {} is {}".format(epoch + 1, model.optimizer.lr.numpy()))
+
 if __name__ == "__main__":
+  # Define distribution stategy
+  strategy = tf.distribute.MirroredStrategy()
+  print("Number of GPUs: ", strategy.num_replicas_in_sync)
+
   # Get classes and paths to images
   class_names, train_files, valid_files = data.load_files(train_path)
 
@@ -18,10 +36,9 @@ if __name__ == "__main__":
   train_ds = data.AlaskaSequence(train_files, class_names, batch_size)
   valid_ds = data.AlaskaSequence(valid_files, class_names, batch_size)
 
-  strategy = tf.distribute.MirroredStrategy()
   with strategy.scope():
     # Load model
-    model = model.get_model(img_width, img_height)
+    model = model.get_model(img_width, img_height, len(class_names))
     print(model.summary())
 
     # Create a callback that saves the model's weights
@@ -40,6 +57,12 @@ if __name__ == "__main__":
     model.load_weights(latest)
     """
 
+    callbacks=[
+      cp_callback,
+      keras.callbacks.LearningRateScheduler(decay),
+      PrintLR()
+    ]
+
     # Start training
     model.fit(
       train_ds,
@@ -47,8 +70,8 @@ if __name__ == "__main__":
       validation_data=valid_ds,
       validation_steps=len(valid_files) // batch_size,
       epochs=epochs,
-      callbacks=[cp_callback],
-      max_queue_size=40,
+      callbacks=callbacks,
+      max_queue_size=256,
       use_multiprocessing=True,
       workers=8
     )
