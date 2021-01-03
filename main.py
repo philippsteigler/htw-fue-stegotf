329bc11974ec667b5bb86514600aa339ebf27686
Philipp Steigler <philipp.steigler@mailbox.org>
2021-01-03 22:57:59 +0100

    Add weighted AUC from competition

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 import pathlib
 import numpy as np
+from sklearn import metrics
 
 import tensorflow as tf
 from tensorflow import keras
 import tensorflow.keras.applications.efficientnet as efn
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
-home_path = "/home/phst757c/ALASKA3"
+home_path = "/home/phst757c/ALASKA2"
 train_path = "/projects/p_ml_steg_steigler/ALASKA2/train"
 
 img_width = 512
@@ -62,8 +63,8 @@ def get_model(num_classes):
     optimizer="adam",
     loss="categorical_crossentropy",
     metrics=[
-      keras.metrics.CategoricalAccuracy(name="CatAcc"),
-      keras.metrics.AUC(name="AUC"),
+      keras.metrics.CategoricalAccuracy(name="Acc"),
+      alaska_weighted_auc,
       keras.metrics.Precision(name="Global Pre"),
       keras.metrics.Recall(name="Global Rec"),
       keras.metrics.Precision(name="Pre C0", class_id=0),
@@ -78,6 +79,36 @@ def get_model(num_classes):
   )
 
   return model
+
+def alaska_weighted_auc(y_true, y_valid):
+  tpr_thresholds = [0.0, 0.4, 1.0]
+  weights =        [       2,   1]
+  
+  fpr, tpr, thresholds = metrics.roc_curve(y_true, y_valid, pos_label=1)
+  
+  # size of subsets
+  areas = np.array(tpr_thresholds[1:]) - np.array(tpr_thresholds[:-1])
+  
+  # The total area is normalized by the sum of weights such that the final weighted AUC is between 0 and 1.
+  normalization = np.dot(areas, weights)
+  
+  competition_metric = 0
+  for idx, weight in enumerate(weights):
+    y_min = tpr_thresholds[idx]
+    y_max = tpr_thresholds[idx + 1]
+    mask = (y_min < tpr) & (tpr < y_max)
+
+    x_padding = np.linspace(fpr[mask][-1], 1, 100)
+
+    x = np.concatenate([fpr[mask], x_padding])
+    y = np.concatenate([tpr[mask], [y_max] * len(x_padding)])
+    y = y - y_min # normalize such that curve starts at y=0
+    score = metrics.auc(x, y)
+    submetric = score * weight
+    best_subscore = (y_max - y_min) * weight
+    competition_metric += submetric
+      
+  return competition_metric / normalization
 
 if __name__ == "__main__":
   # Define distribution stategy
